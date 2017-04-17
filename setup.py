17a5d4e61c425d8aced8cf5bd5162e8a2946665f
James Turk <james.p.turk@gmail.com>
2017-04-17 17:18:14 -0400

    config

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-simplekeys',
+    version="0.1.0",
+    packages=find_packages(),
+    include_package_data=True,
+    description='Django API Key management & validation',
+    author='James Turk',
+    author_email='james.p.turk@gmail.com',
+    license='MIT License',
+    url='http://github.com/jamesturk/django-simplekeys/',
+    long_description="",
+    platforms=["any"],
+    install_requires=[
+        "Django",
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Environment :: Web Environment',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
--- simplekeys/views.py
+++ simplekeys/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def registration():
+    """
+        present user with a form to fill out to get a key
+
+        upon submission, send an email sending user to confirmation page
+    """
+    pass
+
+
+def confirmation():
+    """
+        present user with a simple form that just needs to be submitted
+        to activate the key (don't do activation on GET to prevent
+        email clients from clicking link)
+    """
+    pass
