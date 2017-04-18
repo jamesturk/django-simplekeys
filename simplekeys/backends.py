15be645ced9ba38e926edd43f340d7e491cef909
James Turk <james.p.turk@gmail.com>
2017-04-17 23:22:47 -0400

    restore Django 1.8 compat

--- simplekeys/backends.py
+++ simplekeys/backends.py
@@ -59,7 +59,14 @@ class CacheBackend(AbstractBackend):
 
     def get_tokens_and_timestamp(self, key, zone):
         kz = '{}-{}'.format(key, zone)
-        return self.cache.get_or_set(kz, (0, None), self.timeout)
+        # once we drop Django 1.8 support we can use
+        # return self.cache.get_or_set(kz, (0, None), self.timeout)
+        val = self.cache.get(kz)
+        if val is None:
+            val = self.cache.add(kz, (0, None), timeout=self.timeout)
+            if val:
+                return self.cache.get(kz)
+        return val
 
     def set_token_count(self, key, zone, tokens):
         kz = '{}-{}'.format(key, zone)
@@ -67,5 +74,8 @@ class CacheBackend(AbstractBackend):
 
     def get_and_inc_quota_value(self, key, zone, quota_range):
         quota_key = '{}-{}-{}'.format(key, zone, quota_range)
-        self.cache.get_or_set(quota_key, 0)
+        # once we drop Django 1.8 support we can use
+        # self.cache.get_or_set(quota_key, 0, timeout=self.timeout)
+        if quota_key not in self.cache:
+            self.cache.add(quota_key, 0, timeout=self.timeout)
         return self.cache.incr(quota_key)
