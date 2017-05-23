3d0044130029656c12a4fba5c14f28ae7513f2bd
James Turk <james.p.turk@gmail.com>
2017-05-22 22:42:56 -0700

    switch middleware to use SIMPLEKEYS_ZONES, actually useful

--- simplekeys/middleware.py
+++ simplekeys/middleware.py
@@ -1,16 +1,33 @@
+import re
 from django.conf import settings
 from django.http import JsonResponse
 from .verifier import verify, VerificationError, RateLimitError, QuotaError
 
 
 class SimpleKeysMiddleware(object):
-    def __init__(self, zone=None):
-        if zone is None:
-            self.zone = getattr(settings, 'SIMPLEKEYS_DEFAULT_ZONE', 'default')
-        else:
-            self.zone = zone
+    def __init__(self):
+        self._zones = None
+
+    @property
+    def zones(self):
+        if not self._zones:
+            from django.conf import settings
+            zones = getattr(settings, 'SIMPLEKEYS_ZONES',
+                            [('.*', 'default')])
+
+            self._zones = [
+                ((re.compile(path) if isinstance(path, str) else path), zone)
+                for (path, zone) in zones
+            ]
+        return self._zones
 
     def process_request(self, request):
+        for path, zone in self.zones:
+            if path.match(request.path):
+                break
+        else:
+            return None
+
         key = request.META.get(getattr(settings, 'SIMPLEKEYS_HEADER',
                                        'HTTP_X_API_KEY'))
         if not key:
@@ -18,7 +35,7 @@ class SimpleKeysMiddleware(object):
                                           'apikey'))
 
         try:
-            verify(key, self.zone)
+            verify(key, zone)
         except VerificationError as e:
             return JsonResponse({'error': str(e)}, status=403)
         except RateLimitError as e:
